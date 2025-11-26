391897cd6bbcd322bf724f633c884d9e62631bd3
Bilal Irfan <bi0755562@gmail.com>
2025-11-26 22:13:54 +0500

    Login & Register pages created successfully

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import LoginForm from './LoginForm'
 
 export default function LoginPage() {
   return (
-    <div>
-      LoginPage
+    <div className='flex items-center justify-center h-[calc(100vh-64px)]'>
+      <LoginForm />
     </div>
   )
 }
